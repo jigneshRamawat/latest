5c84f9142a9a519cd1b8d8ff784fc172b42468c9
jignesh <jigneshramawat21@gmail.com>
2026-01-23 16:23:17 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,10 +8,7 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
-    credentials: true,
-}));
+app.use(cors());
 
 app.use(cookieParser());
 app.use(express.json());
